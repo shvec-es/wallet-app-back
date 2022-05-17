d45971a8d2807d3d180882fb873109b25bda4165
Vitaliy Todorov <vitaliitodorov94@gmail.com>
2022-05-17 16:29:14 +0300

    validation done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const { login, logout, signup } = require("../../controllers/authorization.js");
-const { validateAuth } = require("../../middlewares/validation.js");
+const { validateAuth, validateBody, validationSignupUser, validationLoginUser } = require("../../middlewares/validation.js");
 const { controlWrapper } = require("../../middlewares/controlWrapper.js");
-
 
 const router = new express.Router();
 
-router.post('/signup', controlWrapper(signup));
+router.post('/signup', validateBody(validationSignupUser), controlWrapper(signup));
 
-router.post('/login', controlWrapper(login));
+router.post('/login', validateBody(validationLoginUser), controlWrapper(login));
 
 router.get('/logout', validateAuth, controlWrapper(logout));
 
